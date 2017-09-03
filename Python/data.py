736873aed11a705e3e83dd1567a06ce5422be9ee
wenzong <zhou.wenzong@qianka.com>
2017-09-03 21:55:34 +0800

    Update data.py/requirements.txt

--- Python/data.py
+++ Python/data.py
@@ -4,12 +4,32 @@
 import pandas as pd
 import numpy as np
 
+
+# Series
+# one-dimensional labeled array
+#
+# * From Python dict
+# * From ndarray
+# * From scalar value
+s = pd.Series(dict(a=1, b=2, c=3))
+print(s)
+s = pd.Series(dict(a=1, b=2, c=3), ['a', 'b', 'd'])
+print(s)
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
+print(s)
+s = pd.Series([1, 3, 5, np.nan, 6, 8], index=[i for i in 'abcdef'])
+print(s)
+s = pd.Series([i for i in 'fedcba'], index=[i for i in 'abcdef'])
+print(s)
+s = pd.Series(5, index=[i for i in 'abcdef'])
 print(s)
 
 dates = pd.date_range('20160801', periods=6)
 print(dates)
 
+# DataFrame
+# 2-dimensional labeled data structure with columns of potentially different
+# types.
 df2 = pd.DataFrame({
     'A': 1.,
     'B': pd.Timestamp('20160826'),
